9d50dcb024dc21764d8e5a306f08db9a2bcac291
Eli Nihamkin <eli.nihamkin@gmail.com>
2024-11-13 21:51:03 +0200

    full project

---
+++ test_server/test_app/test_app.py
@@ -0,0 +1,37 @@
+import requests
+import os
+
+def test_servers():
+    
+    try:
+        response1 = requests.get('http://nginx_servers:8081')
+        if response1.status_code != 200:
+            print("Custom server test failed")
+            return False
+
+        response2 = requests.get('http://nginx_servers:8082')
+        if response2.status_code != 500:
+            print("Error server test failed")
+            return False
+
+        print("All tests passed successfully")
+        return True
+
+    except Exception as e:
+        print(f"Test failed with error: {e}")
+        return False
+
+
+def main():
+    success = test_servers()
+
+    if success:
+        with open("/result/succeeded", "w"):
+            print("created succeeded file")
+    else:
+        with open("/result/fail", "w"):
+            print("created fail file")
+
+
+if __name__ == "__main__":
+    main()
